74b4c7ed7b20c9024772e70c97c9fd8ba08b9206
zain64-design <84985277+zain64-design@users.noreply.github.com>
2021-05-28 10:27:17 -0700

    plants website

---
+++ js/main.js
@@ -0,0 +1,63 @@
+// $('.owl-carousel').owlCarousel({
+//   loop:true,
+//   margin:10,
+//   nav:true,
+//   dots: true,
+//  navText: [
+//       '<i class="fas fa-chevron-left" aria-hidden="true"></i>',
+//       '<i class="fas fa-chevron-right" aria-hidden="true"></i>'
+//   ],
+// responsiveClass:true,
+//   responsive:{
+//       0:{
+//           items:1
+//       },
+//       600:{
+//           items:1
+//       },
+//    780:{
+//           items:1
+//       },
+//       1000:{
+//           items:1
+//       }
+//   }
+// });
+
+$('#owl-carousel-s1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+
+
+$('#owl-carousel-sc').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
